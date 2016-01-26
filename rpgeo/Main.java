a2d58d4bd6d05adb637bb608e6453e5d2d0c7acf
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-26 04:19:00 -0500

    -additions to Main.java

--- rpgeo/Main.java
+++ rpgeo/Main.java
@@ -1,9 +1,27 @@
 package rpgeo;
+
 import gfm.Game;
+import gfm.Macro;
+import gfm.gamestate.GameState;
 
 public class Main {
+
+   private static int gameWidth = 640;
+   private static int gameHeight = 480;
+
+   private static int drawWidth = (int) (1.5 * gameWidth);
+   private static int drawHeight = (int) (1.5 * gameHeight);
+   
    public static void main(String[] args) {
-      Game game = new Game("RPGeo", 640, 480);
+      Game game = new Game("RPGeo", gameWidth, gameHeight, drawWidth, drawHeight);
+      game.setGameState("test");
+
+      GameState test = new Test(game);
+      game.addGameState(test);
+
+      Macro gameGui = new GameGUI(game);
+      game.addMacro(gameGui);
+
       game.start();
    }
 }
