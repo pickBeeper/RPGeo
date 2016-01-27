84cda7c48cdf672eb1053054a5eba16c09cf6ae7
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-27 02:46:16 -0500

    Test.java reimplemented
    -Add world
    -Remove GUI

--- rpgeo/Test.java
+++ rpgeo/Test.java
@@ -1,7 +1,6 @@
 package rpgeo;
 
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
@@ -10,8 +9,7 @@ import gfm.Game;
 import gfm.gamestate.GameState;
 
 public class Test extends GameState {
-   private Rectangle myPlayBounds;
-   private MouseGoto myMouseGoto;
+   private World myWorld;
 
    public Test(Game game) {
       super(game);
@@ -23,91 +21,78 @@ public class Test extends GameState {
 
    @Override
    public void draw(Graphics pen) {
-      myMouseGoto.draw(pen);
+      myWorld.draw(pen);
+      getGUIManager().draw(pen);
    }
 
    @Override
    public void update() {
-      myMouseGoto.update();
+      myWorld.update();
+      getGUIManager().update();
    }
 
    @Override
    public void initUI() {
-      // TODO Auto-generated method stub
-
    }
 
    @Override
    public void init() {
-      myPlayBounds = new Rectangle();
-      // based on numbers in GameGUI
-      myPlayBounds.setLocation(getWidth() / 6 + 8, 4);
-      myPlayBounds.setSize(getWidth() * 5 / 6 - 8, getHeight() * 4 / 5);
-      myMouseGoto = new MouseGoto(myPlayBounds);
+      myWorld = new World(getGUIManager());
    }
 
    @Override
    public void keyPressed(KeyEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().keyPressed(event);
    }
 
    @Override
    public void keyReleased(KeyEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().keyReleased(event);
    }
 
    @Override
    public void keyTyped(KeyEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().keyTyped(event);
    }
 
    @Override
    public void mouseClicked(MouseEvent event) {
-      myMouseGoto.mouseClicked(event);
+      getGUIManager().mouseClicked(event);
    }
 
    @Override
    public void mouseDragged(MouseEvent event) {
-      myMouseGoto.mouseDragged(event);
+      getGUIManager().mouseDragged(event);
    }
 
    @Override
    public void mouseEntered(MouseEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mouseEntered(event);
    }
 
    @Override
    public void mouseExited(MouseEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mouseExited(event);
    }
 
    @Override
    public void mouseMoved(MouseEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mouseMoved(event);
    }
 
    @Override
    public void mousePressed(MouseEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mousePressed(event);
    }
 
    @Override
    public void mouseReleased(MouseEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mouseReleased(event);
    }
 
    @Override
    public void mouseWheelMoved(MouseWheelEvent event) {
-      // TODO Auto-generated method stub
-
+      getGUIManager().mouseWheelMoved(event);
    }
 
 }
