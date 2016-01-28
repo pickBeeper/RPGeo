d1bd56525cbe1094e822a9a6bf4e179e45a65a48
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-27 22:13:41 -0500

    Refactor KeyEvent Handling in ChatBox.java

--- rpgeo/ChatBox.java
+++ rpgeo/ChatBox.java
@@ -5,37 +5,61 @@ import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.util.LinkedList;
 
+import gfm.gui.GUIBox;
+
 public class ChatBox extends GUIBox {
    private Player myPlayer;
    private LinkedList<ChatMessage> myMessages;
    private String myCurrentMessage;
 
    public ChatBox(Rectangle box, Color color, Player player) {
-      super(box, color);
+      super(box, color, color.getAlpha() / 3, color.getAlpha(), 3);
       myPlayer = player;
       myMessages = new LinkedList<ChatMessage>();
       myCurrentMessage = "";
    }
 
+   @Override
    public void keyPressed(KeyEvent event) {
       int code = event.getKeyCode();
+      checkToActivate(code);
+      checkToSend(code);
+      checkToAppend(code);
+   }
 
-      if ( code == KeyEvent.VK_SLASH && !getIsActivated() ) {
+   public void checkToActivate(int keyCode) {
+      if ( keyCode == KeyEvent.VK_SLASH && !isActivated() ) {
          activate();
-      } else if ( getIsActivated() ) {
-         if ( code == KeyEvent.VK_ENTER ) {
-            myMessages.add(new ChatMessage(myPlayer, myCurrentMessage));
-            myCurrentMessage = "";
-            deactivate();
-         } else if ( getIsActivated() && KeyEvent.getKeyText(code).length() == 1 ) {
-            myCurrentMessage += event.getKeyChar();
-         }
       }
    }
 
-   public void keyReleased(KeyEvent event) {
+   public void checkToSend(int keyCode) {
+      if ( isActivated() && keyCode == KeyEvent.VK_ENTER ) {
+         // ensure message isn't blank
+         boolean validMessage = false;
+         for ( int i = 0; i < myCurrentMessage.length(); i++ ) {
+            if ( myCurrentMessage.charAt(i) != ' ' ) {
+               validMessage = true;
+               break;
+            }
+         }
+
+         // send message if valid
+         if ( validMessage ) {
+            myMessages.add(new ChatMessage(myPlayer, myCurrentMessage));
+         }
+
+         // reset message
+         myCurrentMessage = "";
+         deactivate();
+      }
    }
-   public void keyTyped(KeyEvent event) {
+
+   public void checkToAppend(int keyCode) {
+      String entered = KeyEvent.getKeyText(keyCode);
+      if ( isActivated() && entered.length() == 1 ) {
+         myCurrentMessage += entered;
+      }
    }
 }
 
