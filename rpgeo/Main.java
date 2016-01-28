a82296a06a8922734017d508d95a18b4a073e4d1
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-27 22:16:17 -0500

    Cleanup Main.java

--- rpgeo/Main.java
+++ rpgeo/Main.java
@@ -1,27 +1,21 @@
 package rpgeo;
 
 import gfm.Game;
-import gfm.Macro;
 import gfm.gamestate.GameState;
 
 public class Main {
-
    private static int gameWidth = 640;
    private static int gameHeight = 480;
 
    private static int drawWidth = (int) (1.5 * gameWidth);
    private static int drawHeight = (int) (1.5 * gameHeight);
-   
+
    public static void main(String[] args) {
       Game game = new Game("RPGeo", gameWidth, gameHeight, drawWidth, drawHeight);
-      game.setGameState("test");
 
-      GameState test = new Test(game);
-      game.addGameState(test);
-
-      Macro gameGui = new GameGUI(game);
-      game.addMacro(gameGui);
-
+      GameState play = new Play(game);
+      game.addGameState(play);
+      game.setGameState("play");
       game.start();
    }
 }
